543a8f3d7ef7b708cb550a2051a1ad3dcfe4c426
Erich <eebbinghaus@gmail.com>
2023-09-20 19:42:54 -0600

    created routes and auth

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js"
+import commentRoutes from "./routes/comments.js"
+import videoRoutes from "./routes/videos.js"
+import authRoutes from "./routes/auth.js"
 
 const app = express();
 
@@ -16,6 +20,12 @@ const connect = () => {
             throw err;
         });
 };
+
+app.use(express.json())
+app.use("/api/users", userRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/videos", videoRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(8800, () => {
     connect();
