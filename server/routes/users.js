d675bad6e738687ba696a2269084ec1a5b9c774a
Erich <eebbinghaus@gmail.com>
2023-09-22 19:08:59 -0600

    created User routes and controllers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,29 @@
 import express from "express";
-import {  } from "../controllers/user.js";
+import {
+    update,
+    deleteUser,
+    unsubscribe,
+    subscribe,
+    getUser,
+    like,
+    dislike,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+router.put("/:id", verifyToken, update);
 
+router.delete("/:id", verifyToken, deleteUser);
+
+router.get("/find/:id", getUser);
+
+router.put("/sub/:id", verifyToken, subscribe);
+
+router.put("/unsub/:id", verifyToken, unsubscribe);
+
+router.put("/like/:videoId", verifyToken, like);
+
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
